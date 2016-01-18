10435a2e8c9160b431310e0c431f74ea1acee0d6
sunnyjoe <jiaoqingsunny@gmail.com>
2016-01-18 18:29:10 +0800

    follow to page 35

--- AR_Chapter1/AR_Chapter1/ViewController.swift
+++ AR_Chapter1/AR_Chapter1/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationTV = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        CheckHardWare.camera()
-        CheckHardWare.magnetometer()
-        CheckHardWare.audio()
+
+        locationTV.text = "Good"
+        locationTV.font = UIFont.boldSystemFontOfSize(14)
+        locationTV.frame = CGRectMake(20, 100, view.frame.size.width - 40, 200)
+        view.addSubview(locationTV)
+        
+        let locationMg = CLLocationManager()
+        locationMg.delegate = self
+        locationMg.desiredAccuracy = kCLLocationAccuracyBest
+        locationMg.distanceFilter = 500
+        locationMg.startUpdatingLocation()
     }
-
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        locationTV.text = "Latitude \(newLocation.coordinate.latitude) \nLongitude \(newLocation.coordinate.longitude) \nHeight \(newLocation.verticalAccuracy)"
+    }
  
-
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        for location in locations{
+            locationTV.text = "Latitude \(location.coordinate.latitude) \nLongitude \(location.coordinate.longitude) \nHeight \(location.verticalAccuracy)"
+        }
+    }
 }
 
